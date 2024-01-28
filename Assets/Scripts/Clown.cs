b44dfc6892af859d1a059b054e9cc5514c39add0
LouisVanhove <vanhovelouis123@gmail.com>
2024-01-28 10:13:47 +0100

    maze import and clown animation test

--- Assets/Scripts/Clown.cs
+++ Assets/Scripts/Clown.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Clown : MonoBehaviour // add an audiosource to the clown!
 {
+    public Animator Animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Animator.SetBool("IsChasing", true);
+        }
     }
 }
